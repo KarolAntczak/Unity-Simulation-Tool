09b16df2fad80d3978f6fb0594a8d0542dc02509
Karol <karolczak0@gmail.com>
2016-11-25 19:37:29 +0100

    Selecting objects.

---
+++ Assets/Scripts/SelectObject.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Displays selection when clicking an object
+/// </summary>
+public class SelectObject : MonoBehaviour {
+
+    public GameObject SelectionPrefab;
+    private GameObject selectionInstance;
+
+    private bool selected = false;
+
+    void Update()
+    {
+        if (selected)
+        {
+            if (selectionInstance == null)
+            {
+                selectionInstance = Instantiate(SelectionPrefab);
+                selectionInstance.transform.SetParent(transform);
+                selectionInstance.transform.localPosition = Vector3.zero;
+            }
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(selectionInstance);
+        }
+
+        selected = false;
+    }
+
+    void OnMouseDown()
+    {
+        selected = true;
+    }
+
+}
