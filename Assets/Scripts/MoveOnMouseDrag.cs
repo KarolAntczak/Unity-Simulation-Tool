2341be85b2bb1e426d797a95b6f3d42892affd78
Karol <karolczak0@gmail.com>
2016-11-25 18:46:08 +0100

    Blueprint

---
+++ Assets/Scripts/MoveOnMouseDrag.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Moves object on mouse dragging
+/// </summary>
+public class MoveOnMouseDrag : MonoBehaviour {
+
+    public float Speed = 1;
+
+    bool isDragging = false;
+    Vector3 lastPosition;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            isDragging = true;
+            lastPosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            isDragging = false;
+        }
+
+        if (isDragging)
+        {
+            Vector3 delta = (Input.mousePosition - lastPosition)* Speed;
+            
+            transform.Translate(delta.x,0,delta.y);
+            Debug.Log(delta);
+            lastPosition = Input.mousePosition;
+        }
+    }
+
+}
