19f49f7170261066fb043da2af08356051863846
Karol <karolczak0@gmail.com>
2016-11-26 11:43:32 +0100

    Context menu.

---
+++ Assets/Scripts/ShowContextMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Displays context menu when right-clicking the object
+/// </summary>
+public class ShowContextMenu : MonoBehaviour {
+
+    public GameObject ContextMenu;
+
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+            if (Physics.Raycast(ray, out hit))
+            {
+                ContextMenu.SetActive(true);
+                ContextMenu.transform.position = Input.mousePosition;
+            }
+        } else if (Input.GetMouseButtonDown(0))
+        {
+            ContextMenu.SetActive(false);
+        }
+    }
+}
