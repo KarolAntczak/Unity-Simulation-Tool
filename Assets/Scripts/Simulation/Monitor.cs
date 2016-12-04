a99c21157b1f42ea5c0e87341d7e61ff7b57c4e0
Karol <karolczak0@gmail.com>
2016-12-04 20:25:29 +0100

    Monitors.

---
+++ Assets/Scripts/Simulation/Monitor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Gathers statistics about processed requests.
+/// </summary>
+public class Monitor : Node {
+
+    public float RequestsProcessed = 0;
+    public float RequestsIntensity = 0;
+
+    public override void Process(Request request)
+    {
+        RequestsProcessed++;
+        RequestsIntensity = RequestsProcessed / Time.fixedTime;
+    }
+
+}
