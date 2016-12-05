e72b33b70978c46cc5b15f1c7dabf8af71db1817
Rijandel <lukas9119@gmail.com>
2016-12-05 23:44:50 +0100

    Router node

---
+++ Assets/Scripts/Routing/IRouting.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Routing strategy
+/// </summary>
+public interface IRouting<T> {
+
+    /// <summary>
+    /// Get next element based on routing strategy.
+    /// </summary>
+    T GetNextElement(List<T> list);
+}
---
+++ Assets/Scripts/Simulation/Router.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Router : Node {
+
+    public IRouting<Connection> RoutingStrategy = new RoundRobinRouting<Connection>();
+
+    public override void Process(Request request)
+    {
+        request.Redirect(RoutedConnection);
+    }
+
+    public Connection RoutedConnection
+    {
+        get
+        {
+            List<Connection> connections = OutgoingConnections;
+            return RoutingStrategy.GetNextElement(connections);
+        }
+    }
+}
